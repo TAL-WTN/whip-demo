325acc2699766b061221929b96e12b3a2c4cdbfc
zhangwenbo1 <zhangwenbo1@tal.com>
2023-04-25 11:00:24 +0800

    Fix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useState, memo, useMemo} from 'react';
 import {
   Button,
   Space,
@@ -9,301 +9,38 @@ import {
 } from "@arco-design/web-react";
 import { v4 as uuid } from "uuid";
 import {generateToken, getUrlPrmt} from './util';
-// import {useSubscribe} from './whip-sdk-react/dist/index';
-// import useSubscribe from './UseSubscribe';
 import Subscriber from './Subscriber';
+import Publisher from './Subscriber';
 import './App.css';
-import getToken from './getToken';
+import {IconRefresh, IconVideoCamera, IconVoice} from '@arco-design/web-react/icon';
 
 function App() {
   const queryObject = getUrlPrmt();
 
-  const [ mode, setMode ] = useState("pull");
-  const [ Domain, setDomain ] = useState(queryObject.Domain || window.location.host);
+  const [ mode, setMode ] = useState("push");
   const [ AppID, setAppID ] = useState(queryObject.AppID || "bc22d5");
   const [ AppKey, setAppKey ] = useState(queryObject.AppKey || "00eec858271ea752");
   const [ StreamID, setStreamID ] = useState(queryObject.StreamID || uuid());
-  const [ SessionID, setSessionID] = useState(queryObject.SessionID || uuid());
-  const [ pullUrl, setPullUrl ] = useState("");
-  const [ pushUrl, setPushUrl ] = useState(window.location.origin);
-  const [ visibility, setVisibility ] = useState(false);
-  const [ MuteAudio, setMuteAudio ] = useState(false);
-  const [ MuteVideo, setMuteVideo ] = useState(false);
-  const [ PullAuth, setPullAuth ] = useState(true);
-  const [ ClientIp, setClientIp ] = useState("");
-  const [ iceState, setIceState ] = useState("");
-  const [ dtlsAudioState, setDtlsAudioState ] = useState("");
-  const [ dtlsVideoState, setDtlsVideoState ] = useState("");
-  const [ captureResolution, setCaptureResolution ] = useState("");
-  const [ resolution, setResolution ] = useState("");
-  const [ frameRate, setFrameRate ] = useState("");
-  const [ codeRate, setCodeRate ] = useState("");
-  const [ volume, setVolume ] = useState("");
-  const [ parameter, setParameter ] = useState("");
-  const [ requestList, setRequestList ] = useState([]);
-  const [ errorMessage, setErrorMessage ] = useState("");
-  const [ token, setToken ] = useState<string>();
 
-  const startPull = useCallback(async () => {
-    const pullToken = await generateToken({AppID, AppKey, StreamID, Action: 'sub'});
-    setToken(pullToken);
-    setVisibility(true);
-  }, [AppID, AppKey, StreamID]);
+  const [ token, setToken ] = useState("");
 
-  const handleMute = useCallback((mute: boolean, kind: 'video' | 'audio') => {
-    switch (kind) {
-      case 'video':
-        setMuteVideo(mute);
-        break;
-      case 'audio':
-        setMuteAudio(mute);
-        break;
+  const streamContainer = useMemo(() => {
+    if (mode === 'push') {
+      return <Publisher appId={AppID} streamId={StreamID} appKey={AppKey} token={token} ></Publisher>
+    } else {
+      return <Subscriber appId={AppID} streamId={StreamID} appKey={AppKey} token={token} ></Subscriber>
     }
-  }, [StreamID]);
+  }, [mode, AppID, AppKey, StreamID, token])
+
+
   return (
     <div className="Page">
       <PageHeader title="Welcome to the WTN Demo" className="Header" />
       <div className="Contrainer">
-        <div className="Contrainer-left">
-          <Space direction="vertical" size="medium">
-            <div>
-              <div className="Basic-message">
-                <span>Domain: </span>
-              </div>
-              <Input
-                id="Domain"
-                onChange={(value) => {
-                  setDomain(value);
-                }}
-                value={Domain}
-                placeholder="请输入Domain"
-                style={{ width: 300 }}
-              />
-            </div>
-            <div>
-              <div className="Basic-message">
-                <span>AppID：</span>
-              </div>
-              <Input
-                id="AppID"
-                onChange={(value) => {
-                  setAppID(value);
-                }}
-                value={AppID}
-                placeholder="请输入AppID"
-                style={{ width: 300 }}
-              />
-            </div>
-            <div>
-              <div className="Basic-message">
-                <span>AppKey：</span>
-              </div>
-              <Input
-                id="AppKey"
-                onChange={(value) => {
-                  setAppKey(value);
-                }}
-                value={AppKey}
-                placeholder="请输入AppKey"
-                style={{ width: 300 }}
-              />
-            </div>
-
-            <div>
-              {/* <Space> */}
-              <div className="Basic-message">
-                <span>StreamID：</span>
-              </div>
-              <Input
-                id="StreamID"
-                style={{ width: 300, marginRight: 10 }}
-                value={StreamID}
-                onChange={(v) => setStreamID(v)}
-                placeholder="StreamID"
-              />
-              <Button
-                id="StreamID_Random"
-                onClick={() => setStreamID(uuid())}
-              >
-                Random
-              </Button>
-            </div>
-            <div>
-              <div className="Basic-message">
-                <span>SessionID:</span>
-              </div>
-              <Input
-                id="SessionID"
-                style={{ width: 300, marginRight: 10 }}
-                value={SessionID}
-                onChange={(v) => setSessionID(v)}
-                placeholder="SessionID"
-              />
-              <Button
-                id="SessionID_Random"
-                onClick={() => setSessionID(uuid())}
-              >
-                Random
-              </Button>
-              {/* </Space> */}
-            </div>
-            {mode === "push" ? (
-              <Space direction="vertical">
-                <Space>
-                  <Button
-                    id="StartPush"
-                    disabled={visibility}
-                    type="primary"
-                    onClick={() => {
-                      // this.startPush();
-                    }}
-                  >
-                    StartPush
-                  </Button>
-                  <Button
-                    id="StopPush"
-                    disabled={!visibility}
-                    status="danger"
-                    onClick={() => {
-                      // this.stop();
-                    }}
-                  >
-                    StopPush
-                  </Button>
-                  {mode === "push" ? (
-                    <Space>
-                      <Button
-                        id="PullLink"
-                        disabled={!visibility}
-                        onClick={() => {
-                          window.open(pullUrl);
-                        }}
-                      >
-                        PullLink
-                      </Button>
-                      <Button
-                        id="CurrentPushLink"
-                        disabled={!visibility}
-                        onClick={() => {
-                          window.open(pushUrl);
-                        }}
-                      >
-                        CurrentPushLink
-                      </Button>
-                    </Space>
-                  ) : (
-                    <div></div>
-                  )}
-                </Space>
-              </Space>
-            ) : (
-              <Space>
-                <Button
-                  id="StratPull"
-                  disabled={visibility}
-                  type="primary"
-                  onClick={() => {
-                    startPull();
-                  }}
-                >
-                  Start Pull
-                </Button>
-                <Button
-                  id="StopPull"
-                  disabled={!visibility}
-                  status="danger"
-                  onClick={() => {
-                    setToken(undefined);
-                    setVisibility(false);
-                    // subscriber && subscriber.stop();
-                  }}
-                >
-                  Stop Pull
-                </Button>
-              </Space>
-            )}
-          </Space>
-        </div>
-        {/* <div className="Contrainer-right"> */}
-        {/* <div
-            className="Contrainer-right-video"
-            ref={(r) => (this.videoRenderDom = r)}></div> */}
-        <div
-          style={{
-            width: 680,
-            height: 365,
-            display: "flex",
-            flexWrap: "wrap",
-            background: "#D3D3D3",
-            marginTop: -55,
-          }}
-        >
-          {/*<div*/}
-          {/*  style={{ width: 480, height: 375, marginTop: -5 }}*/}
-          {/*  // ref={(r) => (this.videoRenderDom = r)}*/}
-          {/*></div>*/}
-          {token && mode === 'pull' ? <Subscriber token={token} MuteVideo={MuteVideo} MuteAudio={MuteAudio}/> : <></>}
-
-          <div style={{ width: 200, height: 360 }}>
-            <div
-              className="Video-info"
-              style={{
-                marginTop: -14,
-                marginLeft: 20,
-                padding: 10,
-                paddingTop: 0,
-              }}
-            >
-              <p>
-                <b>Info: </b>
-              </p>
-              <p id="ICE">Conn State：{iceState}</p>
-              <p id="Resolution">
-                Resolution：
-                {resolution === "undefined*undefined"
-                  ? "读取中"
-                  : resolution}
-              </p>
-              <p id="FrameRate">
-                Frame Rate：
-                {frameRate === "undefined"
-                  ? "读取中"
-                  : frameRate}
-              </p>
-              <p id="VideoBitrate">Video Bitrate：{codeRate}</p>
-              <p id="Volume">Volume：{volume}</p>
-              <p id="ErrorMessage" style={{ color: "red" }}>
-                {errorMessage
-                  ? `错误信息：${errorMessage}`
-                  : ""}
-              </p>
-            </div>
-          </div>
-          <div>
-            <Space>
-              MuteVideo：
-              <Switch
-                id="MuteVideo"
-                checked={MuteVideo}
-                onChange={async (v) => {
-                  handleMute(v, 'video');
-                }}
-              ></Switch>
-              MuteAudio：
-              <Switch
-                id="MuteAudio"
-                checked={MuteAudio}
-                onChange={async (v) => {
-                  handleMute(v, 'audio');
-                }}
-              ></Switch>
-            </Space>
-          </div>
-        </div>
-        {/* </div> */}
+        { token ? streamContainer : <></>}
       </div>
     </div>
   );
 }
 
-export default App;
+export default memo(App);
--- src/Subscriber.tsx
+++ src/Subscriber.tsx
@@ -1,28 +1,65 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {memo, useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {useSubscribe} from 'whip-sdk-react';
 import './App.css';
+import {Button, Input, Space} from '@arco-design/web-react';
+import {IconRefresh, IconVideoCamera, IconVoice} from '@arco-design/web-react/icon';
+import {v4 as uuid} from 'uuid';
+import {generateToken} from './util';
 
-const Subscriber = (props: { token: string, MuteVideo: boolean, MuteAudio: boolean }) => {
-  const [ MuteAudio, setMuteAudio ] = useState(false);
-  const [ MuteVideo, setMuteVideo ] = useState(false);
+const Subscriber = (props: { streamId: string, appKey: string, appId: string, token: string }) => {
   const [videoStream, setVideoStream] = useState<MediaStream>();
   const subscriber = useSubscribe(props.token);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  // useEffect(() => {
-  //   return () => {
-  //     subscriber.stop();
-  //   };
-  // }, [subscriber])
+  const [ AppID, setAppID ] = useState(props.appId);
+  const [ AppKey, setAppKey ] = useState(props.appKey);
+  const [ StreamID, setStreamID ] = useState(props.streamId || uuid());
+
+  const [ MuteAudio, setMuteAudio ] = useState(false);
+  const [ MuteVideo, setMuteVideo ] = useState(false);
+  const [ PullAuth, setPullAuth ] = useState(true);
+  const [ ClientIp, setClientIp ] = useState("");
+  const [ iceState, setIceState ] = useState("");
+  const [ dtlsAudioState, setDtlsAudioState ] = useState("");
+  const [ dtlsVideoState, setDtlsVideoState ] = useState("");
+  const [ captureResolution, setCaptureResolution ] = useState("");
+  const [ resolution, setResolution ] = useState("");
+  const [ frameRate, setFrameRate ] = useState("");
+  const [ codeRate, setCodeRate ] = useState("");
+  const [ volume, setVolume ] = useState("");
+  const [ parameter, setParameter ] = useState("");
+  const [ requestList, setRequestList ] = useState([]);
+  const [ errorMessage, setErrorMessage ] = useState("");
+  const [ visibility, setVisibility ] = useState(false);
+  const [ token, setToken ] = useState<string>();
+
+
+
+  const handleMute = useCallback((mute: boolean, kind: 'video' | 'audio') => {
+    switch (kind) {
+      case 'video':
+        setMuteVideo(mute);
+        break;
+      case 'audio':
+        setMuteAudio(mute);
+        break;
+    }
+  }, []);
 
   useEffect(() => {
-    setMuteVideo(props.MuteVideo);
-  }, [props.MuteVideo])
+    return () => {
+      subscriber.stop();
+    };
+  }, [props.token])
+
+  useEffect(() => {
+    subscriber.mute(MuteVideo, 'video');
+  }, [MuteVideo])
 
 
   useEffect(() => {
-    setMuteAudio(props.MuteAudio);
-  }, [props.MuteAudio])
+    subscriber.mute(MuteAudio, 'audio');
+  }, [MuteAudio])
 
   useEffect(() => {
     console.log('subscriber.videoTrack', subscriber.videoTrack);
@@ -41,27 +78,130 @@ const Subscriber = (props: { token: string, MuteVideo: boolean, MuteAudio: boole
   }, [videoRef, videoStream]);
 
 
-  const handleMuteRemoteAudio = useCallback((mute: boolean) => {
-    setMuteAudio(mute);
-  }, [MuteAudio]);
-  const handleMuteRemoteVideo = useCallback((mute: boolean) => {
-    setMuteAudio(mute);
-  }, [MuteVideo]);
+
+  const handleSubBtnClick = useCallback(async () => {
+    if(visibility) {
+      setToken('');
+      setVisibility(false);
+    } else {
+      const pullToken = await generateToken({AppID, AppKey, StreamID, Action: 'sub'});
+      setToken(pullToken);
+      setVisibility(true);
+    }
+  }, [AppID, AppKey, StreamID, visibility]);
+
 
   return (
-    <div
-      style={{ width: 480, height: 375, marginTop: -5 }}
-      // ref={(r) => (this.videoRenderDom = r)}
-    >
-      <video
-        className="renderDom"
-        ref={videoRef}
-        autoPlay
-        muted
-        playsInline
-      />
+
+
+    <div className="Contrainer">
+      <div
+        style={{
+          width: 680,
+          height: 450,
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "column",
+          background: "#353231",
+          marginTop: -55,
+        }}
+      >
+        <div style={{position: 'relative', height: 400}}>
+          <div
+            style={{ width: 680, height: 400, marginTop: 0, background: '#000', position: 'absolute' }}
+            // ref={(r) => (this.videoRenderDom = r)}
+          >
+            <video
+              className="renderDom"
+              ref={videoRef}
+              autoPlay
+              muted
+              playsInline
+            />
+          </div>
+
+          <div style={{ width: 200, height: 200, background: 'rgba(0,0,0, .5)', position:'absolute', right: 0 }}>
+            <div
+              className="Video-info"
+              style={{
+
+              }}
+            >
+              <p id="ICE">Conn State：{iceState}</p>
+              <p id="Resolution">
+                Resolution：
+                {resolution === "undefined*undefined"
+                  ? "读取中"
+                  : resolution}
+              </p>
+              <p id="FrameRate">
+                Frame Rate：
+                {frameRate === "undefined"
+                  ? "读取中"
+                  : frameRate}
+              </p>
+              <p id="VideoBitrate">Video Bitrate：{codeRate}</p>
+              <p id="Volume">Volume：{volume}</p>
+              <p id="ErrorMessage" style={{ color: "red" }}>
+                {errorMessage
+                  ? `错误信息：${errorMessage}`
+                  : ""}
+              </p>
+            </div>
+          </div>
+          <div style={{
+            position: 'absolute',
+            right: 10,
+            bottom: 10,
+          }}>
+            <Space>
+              <IconVideoCamera
+                style={{ fontSize: 36, color: MuteVideo ? '#b6b6b6' : '#FFF' }}
+                onClick={() => {
+                  handleMute(!MuteVideo, 'video');
+                }}
+              />
+              <IconVoice
+                style={{ fontSize: 30, color: MuteAudio ? '#b6b6b6' : '#FFF' }}
+                onClick={() => {
+                  handleMute(!MuteAudio, 'audio');
+                }}
+              />
+            </Space>
+          </div>
+        </div>
+
+        <div className="Contrainer-bottom">
+          <Space direction="horizontal" size="medium">
+
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              {/* <Space> */}
+              <div className="Basic-message">
+                <span style={{color: '#FFF'}}>StreamID：</span>
+              </div>
+              <Input
+                id="StreamID"
+                style={{ width: 300, marginRight: 10 }}
+                value={StreamID}
+                onChange={(v) => setStreamID(v)}
+                placeholder="StreamID"
+                suffix={<IconRefresh onClick={() => setStreamID(uuid())} />}
+              />
+            </div>
+            <Button
+              id="StratPull"
+              type="primary"
+              onClick={handleSubBtnClick}
+              style={{backgroundColor: '#b57d70', borderRadius: 5}}
+            >
+              {visibility ? 'Stop Pull' : 'Start Pull'}
+            </Button>
+          </Space>
+        </div>
+      </div>
     </div>
+
   );
 };
 
-export default React.memo(Subscriber);
+export default memo(Subscriber);
